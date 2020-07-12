82f91b1967668f31f7299dfb4b214581dcb9473f
Rebel Soul <mudvari.ankit@gmail.com>
2020-07-11 23:12:15 -0400

    finshed mlab setup for Heroku Deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,10 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+let MONGODB_URI = "mongodb://localhost/budget" || "mongodb://offlinebudgettracker:offlinebudgettracker01@ds031203.mlab.com:31203/heroku_k0v0kwjh"
+mongoose.connect(MONGODB_URI)
 
 // routes
 app.use(require('./routes/api'));
